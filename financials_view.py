120e2c140514364698707f9344155f39a2adf3e0
T-Nai <tananusin@gmail.com>
2025-06-11 11:12:31 +0700

    Update financials_view.py

--- financials_view.py
+++ financials_view.py
@@ -70,25 +70,3 @@ def show_financial_statements_table(df: pd.DataFrame):
     show_cf_statement_table(df)
     show_balance_sheet_table(df)
 
-def show_default_risk_table(df: pd.DataFrame):
-    rows_to_show = [
-        "Quick Ratio",
-        "Current Ratio",
-        "Cash Coverage",
-        "ICR",        
-        "Cash Coverage",
-        "ROE",
-    ]
-
-    # Filter DataFrame
-    df_filtered = df.loc[[row for row in rows_to_show if row in df.index]]
-
-    # Format values
-    def format_val(val):
-        if isinstance(val, (int, float)):
-            return f"{val:,.2f}"
-        return val if val else "-"
-
-    # Display in Streamlit
-    st.dataframe(df_filtered.style.format(format_val))
-

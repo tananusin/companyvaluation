15312bd48573fa5bc315540ad590e3e1486bbbd7
T-Nai <tananusin@gmail.com>
2025-06-11 09:29:45 +0700

    Update financials_view.py

--- financials_view.py
+++ financials_view.py
@@ -106,9 +106,9 @@ def show_balance_sheet_table(df: pd.DataFrame):
     st.dataframe(df_filtered.style.format(format_val))
 
 def show_financial_statements_table(df: pd.DataFrame):
-    show_income_statement_table(df_financials)
-    show_cf_statement_table(df_financials)
-    show_balance_sheet_table(df_financials)
+    show_income_statement_table(df)
+    show_cf_statement_table(df)
+    show_balance_sheet_table(df)
 
 def show_default_risk_table(df: pd.DataFrame):
     rows_to_show = [

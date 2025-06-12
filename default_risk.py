9e9cf0d1d47014fddf7333a9ceef7318b0c0935f
T-Nai <tananusin@gmail.com>
2025-06-12 09:27:22 +0700

    Update default_risk.py

--- default_risk.py
+++ default_risk.py
@@ -3,28 +3,6 @@
 import streamlit as st
 import pandas as pd
 
-def show_default_risk_table(df: pd.DataFrame):
-    rows_to_show = [
-        "Quick Ratio",
-        "Current Ratio",
-        "Cash Coverage",
-        "ICR",        
-        "Cash Coverage",
-        "ROE",
-    ]
-
-    # Filter DataFrame
-    df_filtered = df.loc[[row for row in rows_to_show if row in df.index]]
-
-    # Format values
-    def format_val(val):
-        if isinstance(val, (int, float)):
-            return f"{val:,.2f}"
-        return val if val else "-"
-
-    # Display in Streamlit
-    st.dataframe(df_filtered.style.format(format_val))
-
 def show_supplier_debt_table(df: pd.DataFrame):
     st.markdown("📄 Supplier Debt")
     rows_to_show = [
@@ -54,7 +32,7 @@ def show_financier_debt_table(df: pd.DataFrame):
         "Interest",
         "OCF",
         "ICR",
-        "Cash Coverage",
+        "CF Coverage",
     ]
 
     # Filter DataFrame
